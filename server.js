9508eb6415e93e3384b95351687cbfd1c92de20b
MEENUGU HANI SATWIK <146410982+Satwik-1114@users.noreply.github.com>
2026-01-30 17:11:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -99,7 +99,11 @@ app.post('/retrieve', async (req, res) => {
 
 // ✅ Dynamic port (required for Railway / cloud)
 const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+    res.status(200).send("Backend is running");
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
